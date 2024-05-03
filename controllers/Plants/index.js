43c0e3ffcb9ce877662d73899b44f651217b778f
Lucas Gola da Silva <lucasgola@outlook.com>
2024-05-03 16:06:22 -0300

    Initial folders structure

---
+++ controllers/Plants/index.js
@@ -0,0 +1,9 @@
+import deletePlant from "./deletePlant";
+import registerPlant from "./registerPlant";
+import getPlantInfo from "./getPlantInfo";
+
+export default {
+    deletePlant,
+    registerPlant,
+    getPlantInfo,
+}
---
+++ controllers/analytics/index.js
@@ -0,0 +1,7 @@
+import getData from "./getData"
+import registerAnalysisData from "./registerData"
+
+export default {
+    getData,
+    registerAnalysisData,
+}
