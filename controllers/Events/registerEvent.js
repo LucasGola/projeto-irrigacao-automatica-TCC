42b9092ff090e7d675a21fcb8dca0ce39e37a945
Lucas Gola da Silva <lucasgola@outlook.com>
2024-11-04 19:52:09 -0300

    [add] irrigation interval variable

--- controllers/Events/registerEvent.js
+++ controllers/Events/registerEvent.js
@@ -3,16 +3,20 @@ import models from '../../db/models';
 import { createErrorLog, createActionLog } from '../createLogs';
 
 export default async function registerEvent(req, res) {
-    const { action, plantId, soilHumidity, climateTemperature } = req.body;
+    console.log(" ### POST: Register Event!!")
+
+    const { action, soilHumidity, climateTemperature } = req.body;
 
     try {
         if (
-            _.isNil(plantId) || _.isNil(action) || _.isNil(soilHumidity) ||
+            _.isNil(action) || _.isNil(soilHumidity) ||
             _.isNil(climateTemperature)) throw new Error("Todos os dados são necessários.")
+
+        const activePlant = await models.Plants.findOne({ where: { isActive: true, deletedAt: null } });
 
         await models.sequelize.transaction(async (transaction) => {
             const data = await models.IrrigationLogs.create({
-                plantId,
+                plantId: activePlant.id,
                 action,
                 soilHumidity,
                 climateTemperature
@@ -22,14 +26,14 @@ export default async function registerEvent(req, res) {
 
             if (_.isNil(data)) throw new Error("Não foi possível registrar o evento de irrigação.")
 
-            await createActionLog("Evento de irrigação registrados.", null, null, plantId)
+            await createActionLog("Evento de irrigação registrados.", null, null, activePlant.id)
             return res.status(200).json({
                 message: "Evento de irrigação registrados com sucesso!",
                 data
             })
         });
     } catch (err) {
-        await createErrorLog(err.stack, "Registro de evento de irrigação", plantId)
+        await createErrorLog(err.stack, "Registro de evento de irrigação", null)
         return res.status(500).json({
             message: "Houve um erro ao registrar o evento de irrigação.",
             error: err.message
--- controllers/Plants/getPlantWaterAndTemperatureInfo.js
+++ controllers/Plants/getPlantWaterAndTemperatureInfo.js
@@ -1,23 +1,39 @@
 import _ from "lodash";
 import models from "../../db/models"
 import { createErrorLog, createActionLog } from '../createLogs';
+const { Op } = require('sequelize');
 
 export default async function getPlantWaterAndTemperatureInfo(req, res) {
+    console.log(" ### GET: Plant irrigation configs!!")
+
     try {
-        const data = await models.Plants.findOne({
+        const plantData = await models.Plants.findOne({
             where: {
                 isActive: true,
                 deletedAt: null
             },
-            attributes: ['minWaterPercent', 'idealWaterPercent', 'maxTemperatureClimate', 'minTemperatureClimate'] // TO:DO receber frequência de irrigação
+            attributes: ['id', 'minWaterPercent', 'idealWaterPercent', 'maxTemperatureClimate', 'minTemperatureClimate', 'irrigationFrequency']
         });
 
-        if (_.isNil(data)) throw new Error("Não encontramos nenhuma planta com este ID")
+        if (_.isNil(plantData)) throw new Error("Não encontramos nenhuma planta ativa!")
 
-        await createActionLog("Busca de planta", null, null, data.id)
+        const lastIrrigationDate = await models.IrrigationLogs.findOne({
+            where: {
+                plantId: plantData.id,
+                action: { [Op.or]: ["Planta irrigada com água encanada!", "Planta irrigada com água da chuva!"] }
+            },
+            order: [['createdAt', 'DESC']],
+            attributes: ['createdAt']
+        });
+
+        await createActionLog("Busca de planta", null, null, plantData.id)
+        console.log((lastIrrigationDate == null) ? "none" : lastIrrigationDate.createdAt)
         return res.status(200).json({
             message: "Planta localizada!",
-            data
+            data: {
+                ...plantData.dataValues,
+                lastIrrigation: (lastIrrigationDate == null) ? "none" : lastIrrigationDate.createdAt,
+            }
         })
 
     } catch (err) {
